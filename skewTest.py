7b0efaf9b2345c3745bdde0705f44589c4981bd1
Owen <oreed2@jhu.edu>
2022-05-16 12:31:11 -0400

    implemented more skew tests

--- skewTest.py
+++ skewTest.py
@@ -2,7 +2,10 @@ import data_manager_2 as dm
 
 #init
 LDA_S = dm.LDAManager("Skew_Steep_All_Topics")
-LDA_R = dm.LDAManager("Skew_Steep_All_Topics")
+LDA_R = dm.LDAManager("Regular_Steep_All_Topics")
+
+LDA_S.defaultSetDataGroup()
+LDA_R.defaultSetDataGroup()
 
 #train models
 LDA_S.main(alpha=10,beta=0.05,num_iter=500,K=6,toy_size=100,skew=True)
@@ -37,3 +40,28 @@ LDA_R.produce_csv()
 
 LDA_S.save()
 LDA_R.save()
+
+exit()
+
+#TEST 1 topic w/ different training sizes
+
+size = 50
+for x in range(5):
+	size*=2
+	LDA = dm.LDAManager("Skew_Steep_"+str(x))
+	LDA.setDataGroup("./20news-bydate/20news-bydate-train/sci.electronics")
+	LDA.main(alpha=10,beta=0.05,num_iter=500,K=1,toy_size=size,skew=False)
+	LDA.save()
+
+for x in range(5)
+	LDA = dm.LDAManager("Skew_Steep_"+str(x))
+	LDA.load()
+	LDA.name_topics()
+	LDA.save()
+
+for x in range(5)
+	LDA = dm.LDAManager("Skew_Steep_"+str(x))
+	LDA.initTestSet(100)
+	LDA.labelTestCorpus()
+	LDA.produce_csv()
+	LDA.save()
